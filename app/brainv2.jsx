7c221acd726ec38eaea4d855e6be82a17fa6fdf0
icres <icress26@gmail.com>
2023-07-04 13:28:30 -0600

    Camera rotate on click

--- app/brainv2.jsx
+++ app/brainv2.jsx
@@ -3,38 +3,32 @@ import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Canvas, extend } from '@react-three/fiber'
 import { Suspense } from 'react';
-import { OrbitControls, Effects } from '@react-three/drei';
+import { OrbitControls, Effects, CameraControls } from '@react-three/drei';
 import { UnrealBloomPass } from 'three-stdlib/postprocessing/UnrealBloomPass.cjs'
 import React from 'react';
-import { useState } from 'react'
+import { useRef } from 'react'
 
 extend({ UnrealBloomPass })
 
-
 export default function BrainV2() {
-    const [cam, setCamera] = useState({
-        camPostion: { fov: 75, near: 0.1, far: 1000, position: [0, 1, 3.5] }
-    });
+    const cameraControlRef = useRef(null);
 
-    function zoomIn() {
-        setCamera({
-            ...cam,
-            camPostion: { fov: 75, near: 0.1, far: 1000, position: [0, 1, 2] }
-        });
-        console.log(cam.camPostion)
+    function rotate45() {
+        cameraControlRef.current?.rotate(Math.PI/4, 0, true)
     };
 
     const brain = useLoader(GLTFLoader, './neuronet (BSDF).gltf')
 
     return (
         <div className={styles.canvasContainer}>
-            <button onClick={zoomIn}>Click to Zoom</button>
+            <button onClick={rotate45}>Click to Zoom</button>
             <Canvas
                 className={styles.canvas}
-                camera={cam.camPostion}>
+                camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 3.5] }}>
+                <CameraControls ref={cameraControlRef} />
                 <Suspense fallback={null}>
 
-                    <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={7} />
+                    <OrbitControls enableZoom={false} />
                     <Effects disableGamma>
                         <unrealBloomPass threshold={.5} strength={1.5} radius={0.5} />
                     </Effects>
