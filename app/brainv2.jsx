754916d4e2e2dbb335a0394c0013826fc6d07d97
icres <icress26@gmail.com>
2023-07-08 18:01:48 -0600

    Cypress and trimmed code

--- app/brainv2.jsx
+++ app/brainv2.jsx
@@ -13,25 +13,41 @@ extend({ UnrealBloomPass })
 export default function BrainV2() {
     const cameraControlRef = useRef(null);
 
-    function rotate45() {
-        cameraControlRef.current?.rotate(Math.PI/4, 0, true)
+    function rotateX45() {
+        cameraControlRef.current?.rotate(Math.PI / 4, 0, true)
     };
+
+    function goToBack() {
+        cameraControlRef.current?.setPosition(4.949747468305833, 2.143131898507868e-16, 2.143131898507868e-16, true)
+    }
+
+    function getCurrentValues() {
+        console.log(cameraControlRef.current._camera.position)
+    }
 
     const brain = useLoader(GLTFLoader, './neuronet (BSDF).gltf')
 
     return (
         <div className={styles.canvasContainer}>
-            <button onClick={rotate45}>Click to Zoom</button>
+            <div className={styles.container}>
+                <button className={styles.main_button} onClick={rotateX45}>Rotate X 45</button>
+                <button className={styles.main_button} onClick={goToBack}>Go To Back</button>
+                <button className={styles.main_button} onClick={getCurrentValues}>Get Current Values</button>
+            </div>
+
             <Canvas
                 className={styles.canvas}
                 camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 3.5] }}>
+                    
                 <CameraControls ref={cameraControlRef} />
                 <Suspense fallback={null}>
 
-                    <OrbitControls enableZoom={false} />
                     <Effects disableGamma>
                         <unrealBloomPass threshold={.5} strength={1.5} radius={0.5} />
                     </Effects>
+                    <>
+                    
+                    </>
                     <>
                         <primitive object={brain.scene} scale={.4} />
                     </>
--- app/head.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export function Head() {
-    return(
-        <h1>This is a header</h1>
-    )
-};
