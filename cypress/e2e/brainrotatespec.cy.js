239e6c08be70f5b203537b7e8e9127a38110cc9d
icres <icress26@gmail.com>
2023-07-10 21:55:52 -0600

    Modified test

--- cypress/e2e/brainrotatespec.cy.js
+++ cypress/e2e/brainrotatespec.cy.js
@@ -1,15 +1,19 @@
 describe('buttons', () => {
-  it('loads page', () => {
-    cy.visit('http://localhost:3000/')
-  });
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  })
 
   it('clicks 45 degree button', () => {
-    cy.visit('http://localhost:3000/');
-    cy.get('.page_container___c__5 > :nth-child(1)').wait(3000).click();
+    cy.get('.page_container___c__5 > :nth-child(1)').wait(1000).click().wait(1000);
   });
 
   it('clicks the back button', () => {
-    cy.visit('http://localhost:3000/');
-    cy.get('.page_container___c__5 > :nth-child(2)').wait(3000).click();
+    cy.get('.page_container___c__5 > :nth-child(2)').wait(1000).click().wait(1000);
   });
+
+  it('tests all buttons', () => {
+    cy.get('.page_container___c__5 > :nth-child(1)').wait(1000).click().click();
+    cy.get('.page_container___c__5 > :nth-child(2)').wait(1000).click();
+    cy.get('.page_container___c__5 > :nth-child(1)').wait(1000).click().dblclick();
+  })
 });
