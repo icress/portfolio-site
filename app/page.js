62ea957acc9b4d9b1dc75db73ea1ff9547fc6189
icres <icress26@gmail.com>
2023-07-25 19:39:04 -0600

    Footer

--- app/page.js
+++ app/page.js
@@ -6,6 +6,10 @@ import BrainV2 from './components/brainv2';
 import Card from './components/card';
 import CardGrid from './components/cardGrid';
 import Navbar from './components/navbar';
+import Footer from './components/footer';
+import { Permanent_Marker } from 'next/font/google';
+
+const permMarker = Permanent_Marker({ subsets: ['latin'], weight: '400' });
 
 
 export default function Home() {
@@ -13,36 +17,43 @@ export default function Home() {
     <div className={styles.scene}>
       <Navbar />
       <BrainV2 />
-      <h1 className={styles.header}>Hi, I'm Isaac!</h1>
-      <CardGrid>
-        <Card>
-          <h1>This is a test of the card</h1>
-        </Card>
-        <Card>
-          <h1>This is a test of the card</h1>
-        </Card>
-        <Card>
-          <h1>This is a test of the card</h1>
-        </Card>
-        <Card>
-          <h1>This is a test of the card</h1>
-        </Card>
-        <Card>
-          <h1>This is a test of the card</h1>
-        </Card>
-        <Card>
-          <h1>This is a test of the card</h1>
-        </Card>
-        <Card>
-          <h1>This is a test of the card</h1>
-        </Card>
-        <Card>
-          <h1>This is a test of the card</h1>
-        </Card>
-        <Card>
-          <h1>This is a test of the card</h1>
-        </Card>
-      </CardGrid>
+      <div className={styles.content}>
+        <div className={permMarker.className}>
+          <h1 className={styles.header}>Hi, I'm Isaac</h1>
+        </div>
+
+        <CardGrid>
+          <Card>
+            <h1>This is a test of the card</h1>
+          </Card>
+          <Card>
+            <h1>This is a test of the card</h1>
+          </Card>
+          <Card>
+            <h1>This is a test of the card</h1>
+          </Card>
+          <Card>
+            <h1>This is a test of the card</h1>
+          </Card>
+          <Card>
+            <h1>This is a test of the card</h1>
+          </Card>
+          <Card>
+            <h1>This is a test of the card</h1>
+          </Card>
+          <Card>
+            <h1>This is a test of the card</h1>
+          </Card>
+          <Card>
+            <h1>This is a test of the card</h1>
+          </Card>
+          <Card>
+            <h1>This is a test of the card</h1>
+          </Card>
+        </CardGrid>
+        <Footer />
+      </div>
+      
     </div>
   )
 };
