10e4381ff571132641b7c01ab195851ab05e10fe
icres <icress26@gmail.com>
2023-08-21 19:35:24 -0600

    Updated cards

--- app/page.js
+++ app/page.js
@@ -25,35 +25,33 @@ export default function Home() {
           <h1 className={styles.header}>Hi, I'm Isaac</h1>
         </div>
         <h3 className={styles.disclaimer}>P.S. This website is still a prototype. Completed version will be coming soon!</h3>
+        <h2 className={styles.subheader}>Projects</h2>
         <CardGrid>
           <Card>
-            <h2 className={styles.cardHead}><a href='https://highqualitynonsense.onrender.com/'>High Quality Nonsense</a></h2>
+            <h2 className={styles.cardHead}><a className={styles.cardLink} href='https://highqualitynonsense.onrender.com/'>High Quality Nonsense</a></h2>
             <p className={styles.cardContent}>Blog website for publishing my thoughts, opnions, or random musings</p>
             <strong>Python, HTML, CSS, Jinja, Flask, Bootstrap, SQLite, SQLAlchemy</strong>
           </Card>
           <Card>
-            <h1>This is a test of the card</h1>
-          </Card>
-          <Card>
-            <h1>This is a test of the card</h1>
-          </Card>
-          <Card>
-            <h1>This is a test of the card</h1>
-          </Card>
-          <Card>
-            <h1>This is a test of the card</h1>
-          </Card>
-          <Card>
-            <h1>This is a test of the card</h1>
-          </Card>
-          <Card>
-            <h1>This is a test of the card</h1>
-          </Card>
-          <Card>
-            <h1>This is a test of the card</h1>
-          </Card>
-          <Card>
-            <h1>This is a test of the card</h1>
+            <h2 className={styles.cardHead}><a className={styles.cardLink} href='https://thecoffeespot.onrender.com/'>Coffee Spot</a></h2>
+            <p className={styles.cardContent}>Web app that allows the user to add cafes to a database and see a list of all their cafes. </p>
+            <strong>Python, HTML, CSS, Jinja, Flask, Bootstrap, SQLite, SQLAlchemy</strong>
+          </Card><Card>
+            <h2 className={styles.cardHead}><a className={styles.cardLink} href='https://github.com/icress/job-skill-searcher'>Job Skill Searcher</a></h2>
+            <p className={styles.cardContent}>Automation project scans job postings for specified skills and gathers data to tell which skills are currently in demand. </p>
+            <strong>Python, Selenium</strong>
+          </Card><Card>
+            <h2 className={styles.cardHead}><a className={styles.cardLink} href='https://github.com/icress/youtube-spoiler-filter'>YouTube Spoiler Filter</a></h2>
+            <p className={styles.cardContent}>This project searches YouTube and filters out any videos with titles containing specified words that could lead to spoilers.</p>
+            <strong>Python, Selenium</strong>
+          </Card><Card>
+            <h2 className={styles.cardHead}><a className={styles.cardLink} href='https://icress.github.io/to-do-js/'>To Do List</a></h2>
+            <p className={styles.cardContent}>Simple and stylish to do list</p>
+            <strong>JavaScript, HTML, CSS, Bootstrap</strong>
+          </Card><Card>
+            <h2 className={styles.cardHead}><a className={styles.cardLink} href='https://icress.github.io/scorekeeper/'>Scorekeeper</a></h2>
+            <p className={styles.cardContent}>Web app used for tracking scores</p>
+            <strong>JavaScript, HTML, CSS, Bulma</strong>
           </Card>
         </CardGrid>
         <Footer />
