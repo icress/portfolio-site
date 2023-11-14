8da6fe5bd4fd76e54867bd81b1cdf3d2a5bb75db
icres <icress26@gmail.com>
2023-11-14 16:13:32 -0700

    Flip on click. No back background

--- app/components/box.jsx
+++ app/components/box.jsx
@@ -1,14 +1,32 @@
 import styles from '../box.module.css';
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 
-export default function Box({ children, image, onClick }) {
+export default function Box({ children, image, front, back }) {
+    const [isFlipped, setIsFlipped] = useState(false)
+
+    const handleFlip = () => {
+        setIsFlipped(!isFlipped)
+    }
+
     return (
-        <motion.div 
-        whileHover={{scale: 1.1}}
-        onClick={onClick}
-        style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url(${image})`}} 
-        className={styles.box}>
-            {children}
-        </motion.div>
+        <div className={styles.flipBox} onClick={handleFlip}>
+            <motion.div
+                whileHover={{ scale: 1.1 }}
+                className={styles.box}
+                animate={{rotateY: isFlipped ? 180 : 360}}
+                transition={{duration: 1.2}}>
+                    <div 
+                    className={styles.flipBoxFront}
+                    style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url(${image})` }}>
+                        <h3>{front}</h3>
+                    </div>
+                    <div className={styles.flipBoxBack}>
+                        <p>{back}</p>
+                    </div>
+
+            </motion.div>
+        </div>
+
     );
 };
--- app/components/coreValues.jsx
+++ app/components/coreValues.jsx
@@ -3,18 +3,29 @@ import Box from './box';
 import BoxGrid from './boxGrid';
 
 export default function CoreValues() {
+
+    const values = [{
+        front: 'Constant Learning',
+        image: 'https://images.unsplash.com/photo-1521587760476-6c12a4b040da?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        back: 'This is where the text will go'
+    },
+    {
+        front: 'Ownership',
+        image: 'https://images.unsplash.com/photo-1600132806370-bf17e65e942f?q=80&w=3094&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        back: 'This is the content for the ownership box'
+    },
+    {
+        front: 'Balance',
+        image: 'https://images.unsplash.com/photo-1472494731104-3ba69e52845b?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        back: 'This is the content for the balance box'
+    }]
+
     return (
         <section>
             <BoxGrid>
-                <Box image='https://images.unsplash.com/photo-1521587760476-6c12a4b040da?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
-                    <h3>Constant Learning</h3>
-                </Box>
-                <Box image='https://images.unsplash.com/photo-1600132806370-bf17e65e942f?q=80&w=3094&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
-                    <h3>Ownership</h3>
-                </Box>
-                <Box image='https://images.unsplash.com/photo-1472494731104-3ba69e52845b?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
-                    <h3>Balance</h3>
-                </Box>
+                {values.map((value) => {
+                    return <Box image={value.image} front={value.front} back={value.back}></Box>
+                })}
             </BoxGrid>
         </section>
 
