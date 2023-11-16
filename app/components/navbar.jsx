0d1d00a620e808b8baad8c67756c3276bf4e0957
icres <icress26@gmail.com>
2023-11-16 10:58:51 -0700

    More CSS separation

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,31 +1,12 @@
-import styles from '../page.module.css';
-import { useRef } from 'react';
+import styles from '../navbar.module.css';
 
 export default function Navbar() {
-
-    function rotateX45(cameraControlRef) {
-        cameraControlRef.current?.rotate(Math.PI / 4, 0, true)
-    };
-
-    function goToBack() {
-        cameraControlRef.current?.setPosition(4.949747468305833, 2.143131898507868e-16, 2.143131898507868e-16, true)
-    };
-
-    function getCurrentValues() {
-        console.log(cameraControlRef.current._camera.position)
-    };
 
     return (
         <div className={styles.navbar}>
             <div className={styles.navLeft}>
                 <img className={styles.signature} src='signature-white.svg' onClick='#' />
             </div>
-
-            {/* <div className={styles.navRight}>
-                <button className={styles.mainButton} onClick={rotateX45}>Rotate</button>
-                <button className={styles.mainButton} onClick={goToBack}>Go To Back</button>
-                <button className={styles.mainButton} onClick={getCurrentValues}>Values</button>
-            </div> */}
         </div>
     );
 };
