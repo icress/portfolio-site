1a99ed8b239eb87d2e34e11eb76412a78c4646c0
icres <icress26@gmail.com>
2023-11-22 14:37:27 -0700

    Fixed card link size

--- app/components/card.jsx
+++ app/components/card.jsx
@@ -4,12 +4,12 @@ import { motion } from 'framer-motion'
 
 export default function Card({ children, link }) {
     return (
+        <a className={styles.cardLink} href={link} target='_blank'>
+            <motion.div whileHover={{ scale: 1.1 }} className={styles.card}>
 
-        <motion.div whileHover={{ scale: 1.1 }} className={styles.card}>
-            <a href={link} target='_blank'>
                 {children}
-            </a>
-        </motion.div>
 
+            </motion.div>
+        </a>
     );
 };
