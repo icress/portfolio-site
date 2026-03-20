02cad2e4a848b671d8041e47f7e1d07bb20c48a5
Isaac Cress <icress26@gmail.com>
2026-03-20 12:45:57 -0600

    update test description

--- __tests__/workExperience.test.jsx
+++ __tests__/workExperience.test.jsx
@@ -2,7 +2,7 @@ import WorkExperience from "@/app/components/workExperience"
 import {screen, render} from "@testing-library/react"
 
 describe('Work Expereience', () => {
-    test('fusaware experience', () => {
+    test('software engineer experience', () => {
         render(<WorkExperience />)
         expect(screen.getByText('Software Engineer')).toBeInTheDocument()
     })
